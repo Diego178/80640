a17ddc8498726eaa1e0e284a5bea871e4b50efab
Diego178 <89479225+Diego178@users.noreply.github.com>
2022-10-20 18:03:25 -0500

    BackEnd java

--- Practica06/src/main/java/mx/uv/App.java
+++ Practica06/src/main/java/mx/uv/App.java
@@ -1,5 +1,8 @@
 package mx.uv;
 
+import static spark.Spark.*;
+import com.google.gson.JsonObject;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        get("/hello", (req, res) -> "Hello World");
+
+        get("/lalo", (req, res) -> "Hello Eduardo");
+
+        get("/yo", (req, res) -> "Hello Diego");
+
+        get("/m", (req, res) -> "<h1>Bienvenido</h1><img src='https://www.uv.mx/v2/images/logouv.jpg'>");
+
+        post("/", (req, res)->{
+            System.out.println(req.queryParams("email")+" "+ req.queryParams("password"));
+            System.out.println(req.body());
+            res.status(200);
+            JsonObject oRespuesta = new JsonObject();
+            oRespuesta.addProperty("msj", "hola");
+
+        });
     }
 }
