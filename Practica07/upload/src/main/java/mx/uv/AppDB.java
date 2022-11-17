b2079769796b172bad8d1d7495b65d0d63688692
Diego178 <89479225+Diego178@users.noreply.github.com>
2022-11-16 19:48:06 -0600

    clase 16/11/2022

--- Practica07/upload/src/main/java/mx/uv/AppDB.java
+++ Practica07/upload/src/main/java/mx/uv/AppDB.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+
 import com.google.gson.Gson;
 
 /**
@@ -17,6 +19,7 @@ public class AppDB
     public static Gson gson = new Gson();
     //base de datos en memoria
     public static Map<String, Usuario> usuarios = new HashMap<>();
+    public Conexion c = new Conexion();
     public static void main( String[] args )
     {
 
@@ -29,12 +32,17 @@ public class AppDB
 
         post("/usuarios", (req, res)->{
             String usuario_request = req.body();
-            logger.info(usuario_request);
+            // logger.info(usuario_request);
             Usuario u = gson.fromJson(usuario_request, Usuario.class);
             String id = UUID.randomUUID().toString();
             u.setId(id);
             return "usuarioAgregado";
         });
+        post("/", (req, res)->{
+            String datosFormulario = req.body();
+            Usuario u = gson.fromJson(datosFormulario, Usuario.class);
+            return DAO.crearUsuario(u);
+        })
 
     }
 }
--- Practica07/upload/src/main/java/mx/uv/Conexion.java
+++ Practica07/upload/src/main/java/mx/uv/Conexion.java
@@ -1,5 +1,6 @@
 package mx.uv;
 
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import com.mysql.jdbc.Connection;
@@ -7,17 +8,21 @@ import com.mysql.jdbc.Driver;
 
 public class Conexion {
     private static String url = "jdbc:mysql://127.0.0.1:3306/usuarioDB";
-    private static String driverName = "com.mysql.jdbc.Driver";
+    private static String DriverName = "com.mysql.jdbc.Driver";
     private static String username = "root";
     private static String pass = "1234";
     private static Connection connection = null;
 
     public static Connection getConnection(){
         try{
-            Class.forName(driverName);
-            connection = Driver.getConnection(url, username, pass);
+            Class.forName(DriverName);
+            connection = (Connection)DriverManager.getConnection(url, username, pass);
         }catch (SQLException e){
             System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("No se encontro el driver");
         }
         return connection;
     }
